16910417e5b06b89e9ac78fe52db6e98f1b28b8b
CreepSore <creepsore@gmail.com>
2019-04-03 05:31:19 +0200

    Added Class-System

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
+"use strict";
 let dgram = require("dgram");
 
+let DEFAULT_SERVERDATA;
 let PORT = 27016;
 
 let init = function() {
+    DEFAULT_SERVERDATA = new ServerData();
+    DEFAULT_SERVERDATA.hostname = "[Lambda-Soft] Mari0 Kart 64";
+    DEFAULT_SERVERDATA.gamename = "Mario Kart 64";
+    DEFAULT_SERVERDATA.mapname = "Rainbow Road";
+    DEFAULT_SERVERDATA.playercount = 7;
+    DEFAULT_SERVERDATA.maxplayers = 8;
+    DEFAULT_SERVERDATA.isVAC = false;
+    DEFAULT_SERVERDATA.isPrivate = true;
+
     let sckt = dgram.createSocket("udp4");
-    sckt.bind(PORT, ()=>{});
-    sckt.addListener("message", onPacket)
+    sckt.bind(PORT);
+    sckt.addListener("message", onPacket);
 }
 
 let onPacket = function(msg, remote) {
     if(msg[0] == 0xFF && msg[1] == 0xFF && msg[2] == 0xFF && msg[3] == 0xFF) {
         let pckt = String(msg).substring(4);
-        console.log(remote.address + ":" + remote.port + " -> " + pckt);
+        console.log("[VALID]" + remote.address + ":" + remote.port + " -> " + pckt);
         handlePacket(pckt, remote);
+    } else {
+        console.log("[INVALID]" + remote.address + ":" + remote.port + " -> " + msg);
     }
 }
 
 let handlePacket = function(pckt, remote) {
     let sckt = dgram.createSocket("udp4", () => {});
     if(pckt.startsWith("TSource Engine Query")) {
-        let toSend = constructQueryPacket("test", "test", "test", "", 1337, 69, 88, 0, "w", "d", false, false);
-        console.log(toSend);
+        let toSend = constructQueryPacketByServerData(DEFAULT_SERVERDATA);
         sckt.send(toSend, remote.port, remote.address, (e, b) => {
             if(e) console.log(e);
         });
     }
+}
+
+let constructQueryPacketByServerData = function(serverData) {
+    return constructQueryPacket(serverData.hostname, serverData.mapname, serverData.gamename, 
+        serverData.foldername, serverData.appid, serverData.playercount, serverData.maxplayers,
+        serverData.botcount, serverData.environment, serverData.servertype, serverData.isPrivate,
+        serverData.isVAC);
 }
 
 let constructQueryPacket = function(hostname, mapname, gamename, 
@@ -79,6 +98,24 @@ let constructQueryPacket = function(hostname, mapname, gamename,
     pckt.write(isVAC ? "1" : "0", offs);
 
     return Buffer.from(pckt);
+}
+
+
+class ServerData {
+    constructor() { 
+        this.hostname = "Default Hostname";
+        this.mapname = "Default Mapname";
+        this.gamename = "Default Gamename";
+        this.foldername = "";
+        this.appid = 730;
+        this.playercount = 0;
+        this.maxplayers = 255;
+        this.botcount = 0;
+        this.environment = EnumEnvironments.WINDOWS;
+        this.servertype = EnumServerTypes.DEDICATED;
+        this.isPrivate = false;
+        this.isVAC = true;
+    }
 }
 
 let EnumServerTypes = {
